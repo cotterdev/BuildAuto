e28b853c865a7a5bcb208e4d7fb275ccb3c881b5
miniAVIT-W7-PC\miniAVIT <cotterdev@gmail.com>
2017-03-22 16:03:55 -0400

    Config file needs to be setup so we have source then target, source then target.

--- Build Form/Build Form/Build2.cs
+++ Build Form/Build Form/Build2.cs
@@ -21,7 +21,7 @@ namespace Build_Form
         {
             //string[] fileName = new string[100];
             //= "Test1.txt"
-            string sourcePath = @"C:\_Source";
+            //string sourcePath = @"C:\_Source";
             string targetPath = @"C:\_Target";
 
             //Hey
@@ -41,6 +41,20 @@ namespace Build_Form
             }
 
             // To copy a file to another location and 
+
+            for (int i = 0; i < sourceTargetFileName.Length; i=i+2)
+            {
+                if (sourceTargetFileName[i+1] != null)
+                {
+                    //string sourceFile = System.IO.Path.Combine(sourcePath, sFile);
+                    //string destFile = System.IO.Path.Combine(targetPath, sFile);
+                    //System.IO.File.Copy(sourceFile, destFile, true); //true = // overwrite the destination file if it already exists.
+                    //string destFile = System.IO.Path.Combine(targetPath, "Test1.txt");
+                    System.IO.File.Copy(sourceTargetFileName[i], sourceTargetFileName[i+1], true); //true = // overwrite the destination file if it already exists.
+                }
+            }
+
+            /*
             foreach (string fileConfig in sourceTargetFileName)
             {
                 if (fileConfig != null)
@@ -52,7 +66,7 @@ namespace Build_Form
                     System.IO.File.Copy(fileConfig, destFile, true); //true = // overwrite the destination file if it already exists.
                     Console.WriteLine("Copied {0}", fileConfig);
                 }                
-            }//End foreach
+            }//End foreach*/
 
 
             //Notify user copy is complete
